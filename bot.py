f382d90b9e62d8c13164f57bae58650f21126d47
AVTNET <alejandro.vicente@avtnet.es>
2025-07-25 19:21:15 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")  # Pega aquí el token si no usas v
 CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")  # ID del grupo donde enviar el mensaje
 TIMEZONE = pytz.timezone("Europe/Madrid")
 SEND_HOUR = 19
-SEND_MINUTE = 15
+SEND_MINUTE = 25
 
 MESSAGE = (
     "Buenos días, recordaros que es de obligado cumplimiento el registro horario desde la app de iaccesos. "
